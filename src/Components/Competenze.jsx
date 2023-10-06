b833ae54c4c47803f163a75ce0cb2d6823c3b07b
Andrea Caforio <andreacaforio74@gmail.com>
2023-10-06 21:23:23 +0200

    aggiunto meccanismo competenze

--- src/Components/Competenze.jsx
+++ src/Components/Competenze.jsx
@@ -1,34 +1,135 @@
 import React from "react";
-import { AiFillHtml5 } from "react-icons/ai";
+
 import { FaCss3Alt, FaSass, FaBootstrap } from "react-icons/fa";
 import { BsGit } from "react-icons/bs";
-
+import { useState } from "react";
 import {
   BiLogoJavascript,
   BiLogoReact,
+  BiLogoHtml5,
   BiLogoTypescript,
   BiLogoPostgresql,
   BiLogoJava,
   BiLogoSpringBoot,
   BiLogoRedux
 } from "react-icons/bi";
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
+import { useCallback } from "react";
+
 function Competenze() {
+  const [frontIsHovering, setFrontIsHovering] = useState(false);
+
+  const [whichIsHovering, setWhichIsHovering] = useState(null);
+
+  const handleMouseOver = () => {
+    setFrontIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setFrontIsHovering(false);
+  };
+
+  const handleHovering = (logoName) => {
+    setWhichIsHovering(logoName);
+  };
+
+  const mouseOver = useCallback((logoName) => {
+    handleHovering(logoName);
+    handleMouseOver();
+  }, []);
+
   return (
     <div>
-      <div>Le mie competenze:</div>
-      <div>
-        <AiFillHtml5 />
-        <FaCss3Alt />
-        <BiLogoJavascript />
-        <BiLogoReact />
-        <BiLogoRedux />
-        <FaBootstrap />
-        <FaSass />
-        <BiLogoTypescript />
-        <BiLogoJava />
-        <BiLogoSpringBoot />
-        <BiLogoPostgresql />
-        <BsGit />
+      <div className="allCompetenze">
+        <div>Le mie competenze:</div>
+        <div className={`frontEndSkills box ${frontIsHovering ? "expanded" : ""}`}>
+          <div className="typeContainer">
+            <div className="skillsType">Frontend:</div>
+            <div className="skillsContainer">
+              <BiLogoHtml5
+                id="html5"
+                className="skillIcon"
+                onMouseOver={() => mouseOver("HTML5")}
+                onMouseOut={() => {
+                  handleMouseOut();
+                  handleHovering(null);
+                }}
+              />
+
+              <FaCss3Alt
+                id="css"
+                className="skillIcon"
+                onMouseOver={() => mouseOver("CSS")}
+                onMouseOut={() => {
+                  handleMouseOut();
+                  handleHovering(null);
+                }}
+              />
+              <BiLogoJavascript
+                id="js"
+                className="skillIcon"
+                onMouseOver={() => mouseOver("Javascript")}
+                onMouseOut={() => {
+                  handleMouseOut();
+                  handleHovering(null);
+                }}
+              />
+              <BiLogoReact
+                id="react"
+                className="skillIcon"
+                onMouseOver={() => mouseOver("React")}
+                onMouseOut={() => {
+                  handleMouseOut();
+                  handleHovering(null);
+                }}
+              />
+              <BiLogoRedux
+                id="redux"
+                className="skillIcon"
+                onMouseOver={() => mouseOver("Redux")}
+                onMouseOut={() => {
+                  handleMouseOut();
+                  handleHovering(null);
+                }}
+              />
+              <FaBootstrap
+                id="bootstrap"
+                className="skillIcon"
+                onMouseOver={() => mouseOver("Bootstrap")}
+                onMouseOut={() => {
+                  handleMouseOut();
+                  handleHovering(null);
+                }}
+              />
+              <FaSass
+                id="sass"
+                className="skillIcon"
+                onMouseOver={() => mouseOver("SASS")}
+                onMouseOut={() => {
+                  handleMouseOut();
+                  handleHovering(null);
+                }}
+              />
+            </div>
+          </div>
+          {frontIsHovering ? <div>{whichIsHovering}</div> : ""}
+        </div>
+        <div className="backEndSkills box">
+          <div className="skillsType">Backend:</div>
+          <div className="skillsContainer">
+            <BiLogoTypescript />
+            <BiLogoJava />
+            <BiLogoSpringBoot />
+            <BiLogoPostgresql />
+          </div>
+        </div>
+        <div className="altro box">
+          <div className="skillsType"> Altro:</div>
+          <div className="skillsContainer">
+            {" "}
+            <BsGit />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Components/MyHome.jsx
+++ src/Components/MyHome.jsx
@@ -4,16 +4,18 @@ import laptopImg from "../Assets/laptop.png";
 import HomeContacts from "./HomeComponents/HomeContacts";
 function MyHome() {
   return (
-    <div id="home" className="row">
-      <div className="leftHome col-sm-6">
-        <h1 id="name">
-          <div>Andrea</div>
-          <div className="ms-4 ">Caforio</div>
-        </h1>
-        <h2 className="role">Junior Full Stack Developer</h2>
-      </div>
-      <div className="rightHome col-sm-6">
-        <img src={laptopImg} alt="laptop" />
+    <div>
+      <div id="home" className="row">
+        <div className="leftHome col-sm-6">
+          <h1 id="name">
+            <div>Andrea</div>
+            <div className="ms-4 ">Caforio</div>
+          </h1>
+          <h2 className="role">Junior Full Stack Developer</h2>
+        </div>
+        <div className="rightHome col-sm-6">
+          <img src={laptopImg} alt="laptop" />
+        </div>
       </div>
       <HomeContacts />
     </div>
