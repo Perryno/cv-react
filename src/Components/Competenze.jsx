8f16c23e4738935d2e5f4a8edb7accb16023ee4a
Andrea Caforio <andreacaforio74@gmail.com>
2023-10-07 03:20:17 +0200

    aggiunto script per competenze

--- src/Components/Competenze.jsx
+++ src/Components/Competenze.jsx
@@ -11,9 +11,10 @@ import {
   BiLogoPostgresql,
   BiLogoJava,
   BiLogoSpringBoot,
-  BiLogoRedux
+  BiLogoRedux,
+  BiLogoCss3
 } from "react-icons/bi";
-import { eventWrapper } from "@testing-library/user-event/dist/utils";
+
 import { useCallback } from "react";
 
 function Competenze() {
@@ -38,81 +39,114 @@ function Competenze() {
     handleMouseOver();
   }, []);
 
+  const hideOrNot = (logoName) => {
+    if (whichIsHovering === logoName) {
+      return "show";
+    } else {
+      return "hide";
+    }
+  };
+
   return (
     <div>
       <div className="allCompetenze">
         <div>Le mie competenze:</div>
-        <div className={`frontEndSkills box ${frontIsHovering ? "expanded" : ""}`}>
+        <div
+          className={`frontEndSkills box
+        ${frontIsHovering ? "expanded" : "notExpanded"} `}
+        >
           <div className="typeContainer">
             <div className="skillsType">Frontend:</div>
             <div className="skillsContainer">
-              <BiLogoHtml5
-                id="html5"
-                className="skillIcon"
-                onMouseOver={() => mouseOver("HTML5")}
-                onMouseOut={() => {
-                  handleMouseOut();
-                  handleHovering(null);
-                }}
-              />
+              <div className="iconSpan">
+                <BiLogoHtml5
+                  id="html5"
+                  className="skillIcon"
+                  onMouseOver={() => mouseOver("HTML5")}
+                  onMouseOut={() => {
+                    handleMouseOut();
+                    handleHovering(null);
+                  }}
+                />
 
-              <FaCss3Alt
-                id="css"
-                className="skillIcon"
-                onMouseOver={() => mouseOver("CSS")}
-                onMouseOut={() => {
-                  handleMouseOut();
-                  handleHovering(null);
-                }}
-              />
-              <BiLogoJavascript
-                id="js"
-                className="skillIcon"
-                onMouseOver={() => mouseOver("Javascript")}
-                onMouseOut={() => {
-                  handleMouseOut();
-                  handleHovering(null);
-                }}
-              />
-              <BiLogoReact
-                id="react"
-                className="skillIcon"
-                onMouseOver={() => mouseOver("React")}
-                onMouseOut={() => {
-                  handleMouseOut();
-                  handleHovering(null);
-                }}
-              />
-              <BiLogoRedux
-                id="redux"
-                className="skillIcon"
-                onMouseOver={() => mouseOver("Redux")}
-                onMouseOut={() => {
-                  handleMouseOut();
-                  handleHovering(null);
-                }}
-              />
-              <FaBootstrap
-                id="bootstrap"
-                className="skillIcon"
-                onMouseOver={() => mouseOver("Bootstrap")}
-                onMouseOut={() => {
-                  handleMouseOut();
-                  handleHovering(null);
-                }}
-              />
-              <FaSass
-                id="sass"
-                className="skillIcon"
-                onMouseOver={() => mouseOver("SASS")}
-                onMouseOut={() => {
-                  handleMouseOut();
-                  handleHovering(null);
-                }}
-              />
+                <span className={hideOrNot("HTML5")}>HTML5</span>
+              </div>
+
+              <div className="iconSpan">
+                <BiLogoCss3
+                  id="css"
+                  className="skillIcon"
+                  onMouseOver={() => mouseOver("CSS")}
+                  onMouseOut={() => {
+                    handleMouseOut();
+                    handleHovering(null);
+                  }}
+                />
+                <span className={hideOrNot("CSS")}>CSS</span>
+              </div>
+              <div className="iconSpan">
+                <BiLogoJavascript
+                  id="js"
+                  className="skillIcon"
+                  onMouseOver={() => mouseOver("Javascript")}
+                  onMouseOut={() => {
+                    handleMouseOut();
+                    handleHovering(null);
+                  }}
+                />
+                <span className={hideOrNot("Javascript")}>JavaScript</span>
+              </div>
+              <div className="iconSpan">
+                <BiLogoReact
+                  id="react"
+                  className="skillIcon"
+                  onMouseOver={() => mouseOver("React")}
+                  onMouseOut={() => {
+                    handleMouseOut();
+                    handleHovering(null);
+                  }}
+                />
+                <span className={hideOrNot("React")}>React</span>
+              </div>
+              <div className="iconSpan">
+                <BiLogoRedux
+                  id="redux"
+                  className="skillIcon"
+                  onMouseOver={() => mouseOver("Redux")}
+                  onMouseOut={() => {
+                    handleMouseOut();
+                    handleHovering(null);
+                  }}
+                />
+                <span className={hideOrNot("Redux")}>Redux</span>
+              </div>
+
+              <div className="iconSpan">
+                <FaBootstrap
+                  id="bootstrap"
+                  className="skillIcon"
+                  onMouseOver={() => mouseOver("Bootstrap")}
+                  onMouseOut={() => {
+                    handleMouseOut();
+                    handleHovering(null);
+                  }}
+                />
+                <span className={hideOrNot("Bootstrap")}>Bootstrap</span>
+              </div>
+              <div className="iconSpan">
+                <FaSass
+                  id="sass"
+                  className="skillIcon"
+                  onMouseOver={() => mouseOver("SASS")}
+                  onMouseOut={() => {
+                    handleMouseOut();
+                    handleHovering(null);
+                  }}
+                />
+                <span className={hideOrNot("SASS")}>SASS</span>
+              </div>
             </div>
           </div>
-          {frontIsHovering ? <div>{whichIsHovering}</div> : ""}
         </div>
         <div className="backEndSkills box">
           <div className="skillsType">Backend:</div>
